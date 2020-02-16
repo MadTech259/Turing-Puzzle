f382704b600862141954fa39dd7662414d8de3d8
@ManuBenito98 <manuelbb@hotmail.es>
2020-02-16 18:10:44 -0300

    Basic scripts implementation

---
+++ Assets/Scripts/Symbols Array.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymbolsArray : MonoBehaviour
+{
+    public Symbol[] Symbols { get; private set; }
+
+
+    public Symbol GetSymbolAt(int i)
+    {
+        return Symbols[i];
+    }
+
+    public void WriteSymbolAt(int i, Symbol s)
+    {
+        Symbols[i] = s;
+    }
+}
